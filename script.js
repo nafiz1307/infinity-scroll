e0ac39697aaeb003e94627d4d66e13137224bd36
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-08-30 13:39:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+//Helper Function
+function setAttributes(element,attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 //Create Elements for links and photos
 function displayPhotos(){
     photosArray.forEach((photo)=>{
         const item = document.createElement('a');
-        item.setAttribute('href',photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href',photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item,{
+            href:photo.links.html,
+            target : '_blank',
+        })
 
         const img = document.createElement('img');
-        img.setAttribute('src',photo.urls.regular);
-        img.setAttribute('alt',photo.alt_description);
-        img.setAttribute('title',photo.alt_description);
-
-
+        // img.setAttribute('src',photo.urls.regular);
+        // img.setAttribute('alt',photo.alt_description);
+        // img.setAttribute('title',photo.alt_description);
+        setAttributes(img,{
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
         item.appendChild(img);
         imageContainer.appendChild(item)
     })
